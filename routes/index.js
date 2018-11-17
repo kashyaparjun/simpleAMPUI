26a50292ce967a81670ca9d8957e2771dd9ef76e
Arjun Kashyap <arjuns062009@gmail.com>
2018-11-18 00:58:59 +0530

    craze

--- routes/index.js
+++ routes/index.js
@@ -8,22 +8,47 @@ videos = [];
 router.get('/', function(req, res, next) {
   res.render('index',
     {
-      user: "To AMP Live List",
-      title:"AMP Live List",
+      user: "Help requests",
+      title:"Help!",
       videos: videos
     });
 });
 
+/*
+data incoming format:
+{
+  "secureID": "secureTY",
+  "location": "Some-String-Here",
+  "userName": "User-Name-Here"
+}
+*/
+
 router.post('/', function(req, res, next) {
-  console.log(req.body.videoID);
-  var t = {
-    id: videos.length + 1,
-    YoutubeID: req.body.YoutubeID,
-    timestamp: moment().unix(),
-    timestampStr: moment().format()
+  if(req.body.secureID=="secureTY"){
+    console.log(req.body.videoID);
+    var t = {
+      id: videos.length + 1,
+      location: req.body.location,
+      userName: req.body.userName,
+      timestamp: moment().unix(),
+      timestampStr: moment().format()
+    }
+    videos.push(t);
+    res.status(200).json({"res": t});
   }
-  videos.push(t);
-  res.status(200).json({"res": t});
+  else{
+    res.status(404);
+  }
+});
+
+router.post('/clear/', (req, res, next) => {
+  if(req.body.secureID=="secureTY"){
+    videos = [];
+    res.status(200).json({"clear": "success"});
+  }
+  else{
+    res.status(404);
+  }
 });
 
 module.exports = router;
