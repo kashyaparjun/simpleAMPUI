9eee87cbef8770de4941dd5e35b7e1cc9cee9a80
Arjun Kashyap <arjuns062009@gmail.com>
2018-11-21 11:53:47 +0530

    apple

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+var allowCrossDomain = function(req, res, next) {
+  if ('OPTIONS' == req.method) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
